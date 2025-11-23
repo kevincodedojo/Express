070f844699352f3e1b03216da039d476aa1d0bb0
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-11-23 15:02:24 -0800

    update

--- solarSystem/index.mjs
+++ solarSystem/index.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 const planets = (await import('npm-solarsystem')).default;
 const app = express();
@@ -7,8 +8,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async(req, res) => {
+    // let apiKey = "7756a1e81f817c186cf57294e1c19b37b49c54b8f34e7c499ee0ce5cd86cd16e";
+    // let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&query=solar-system`;
+    // let response = await fetch(url);
+    // let data = await response.json();
+    // let randomImage = data.urls.full;
+    // res.render('index', {"image":randomImage})
+    res.render('index', )
 });
 
 app.get('/earth', (req, res) => {
