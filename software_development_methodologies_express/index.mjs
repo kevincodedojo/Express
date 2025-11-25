7df4245e59c4c6e402d386f16477ce5be3cc235f
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-11-24 16:41:31 -0800

    update agile-simulator table

--- software_development_methodologies_express/index.mjs
+++ software_development_methodologies_express/index.mjs
@@ -14,49 +14,72 @@ app.use(express.static("public"));
 
 //pass currentPath to nav
 app.use((req, res, next) => {
-  res.locals.currentPath = req.path;  
-  next();
+    res.locals.currentPath = req.path;
+    next();
 });
 
-app.get('/', async(req, res) => {
-    
-    res.render('index' )
+app.get('/', async (req, res) => {
+
+    res.render('index');
 });
 
 app.get('/agile', (req, res) => {
-    
-    res.render('agile')
+
+    res.render('agile');
 });
 
 app.get('/scrum', (req, res) => {
-    
-    res.render('scrum')
+
+    res.render('scrum');
 });
 
 app.get('/comparison', (req, res) => {
-    
-    res.render('comparison')
+
+    res.render('comparison');
 });
 
-app.get('/agile-simulator', (req, res) => {
-    
-    
-    res.render('agile-simulator')
+app.get('/agile-simulator', async (req, res) => {
+    //use faker from Node Package to generate fake backlog items
+    const backlogItems = Array.from({ length: 6 }).map(() => ({
+        title: faker.hacker.phrase(),
+        type: faker.helpers.arrayElement(['Feature', 'Bug', 'Task']),
+        assignee: faker.person.fullName(),
+        estimate: faker.number.int({ min: 1, max: 8 }),
+        status: faker.helpers.arrayElement(['To Do', 'In Progress', 'Done'])
+    }));
+
+    //use faker from Node Package to generate fake sprint summary
+    const sprintInfo = {
+        sprintName: `Sprint ${faker.number.int({ min: 1, max: 5 })}`,
+        startDate: faker.date.soon({ days: 1 }),
+        endDate: faker.date.soon({ days: 14 }),
+        scrumMaster: faker.person.fullName()
+    };
+
+    //use Web API for tecky phrase
+    const techyRes = await axios.get('https://techy-api.vercel.app/api/json');
+    const techyPhrase = techyRes.data.message;
+
+    res.render('agile-simulator', {
+        backlogItems,
+        sprintInfo,
+        techyPhrase
+    });
 });
 
 
 // app.get('/nasa', async(req, res) => {
-    
+
 //     let url = `https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=2025-11-11`;
 //     let response = await fetch(url);
 //     let nasaData = await response.json();
 //     res.render('nasa', {nasaData})
-    
+
 // });
 
 // app.get('/mercury', (req, res) => {
 //     let planetMercury = planets.getMercury();
-    
+
 //     res.render('mercury', {planetMercury})
 // });
 
