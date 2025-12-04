c1e1e766d0b91e8d4b11a73736ddc6fe8711dae7
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-12-04 15:25:21 -0800

    2. create home page 3.  create route and view to add authors

---
+++ quoteAdmin/index.mjs
@@ -0,0 +1,74 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//for Express to get values using POST method
+app.use(express.urlencoded({ extended: true }));
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: "o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ugrqthugq1mwg5vc",
+    password: "nv019md5tln5timy",
+    database: "qk970d7iirgndlst",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', (req, res) => {
+    // res.send('Hello Express app!')
+    res.render('index');
+});
+
+//Display form for input Author information
+app.get('/author/new', (req, res) => {
+    
+    res.render('newAuthor');
+});
+
+
+//submitting data to be stored in database
+app.post('/author/new', async(req, res) => {
+
+    let fName = req.body.fName;
+    let lName = req.body.lName;
+    let birthDay = req.body.birthDate;
+    let deathDay = req.body.deathDate;
+    let sex = req.body.sex;
+    let profession = req.body.profession;
+    let country = req.body.country;
+    let portrait = req.body.portrait;
+    let biography = req.body.biography;
+
+    let sql = `INSERT INTO q_authors
+                (firstName, lastName, dob, dod, sex, profession,
+                country, portrait, biography)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                `;
+    let params = [fName, lName, birthDay, deathDay, sex, profession,
+                    country, portrait, biography];
+    const [authors] = await pool.query(sql, params);
+    res.render("newAuthor",
+                {"message": "Author added!"}
+    );  
+    
+
+});
+
+
+
+
+
+
+app.get("/dbTest", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error");
+    }
+});//dbTest
+app.listen(3000, () => {
+    console.log("Express server running")
+})
